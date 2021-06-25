79c189fbf59626c417e29002af3df295cd83ec20
Michael Meinhart <michaelmeinhart@gmail.com>
2021-06-25 15:03:11 -0700

    remove comments from dev url-loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -64,7 +64,8 @@ module.exports = {
                         options: {
                             // On development we want to see where the file is coming from, hence we preserve the [path]
                             name: '[path][name].[ext]?hash=[hash:20]',
-                            limit: 8192
+                            limit: 8192,
+                            removeComments: true
                         }
                     }
                 ]
